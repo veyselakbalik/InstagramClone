2dc5d49d9db9fa31045bcee78bf5615ec2caa536
veyselakbalik <veyselakbalik@ogr.iu.edu.tr>
2023-01-23 19:27:49 +0300

    Create SettingsVC

---
+++ InstaClone/FeedVC.swift
@@ -0,0 +1,29 @@
+//
+//  FeedVC.swift
+//  InstaClone
+//
+//  Created by Veysel Akbalık on 21.01.2023.
+//
+
+import UIKit
+
+class FeedVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ InstaClone/SettingsVC.swift
@@ -0,0 +1,33 @@
+//
+//  SettingsVC.swift
+//  InstaClone
+//
+//  Created by Veysel Akbalık on 21.01.2023.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logOutTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toViewController", sender: nil)
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
